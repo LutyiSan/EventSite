c7c58e1a2ccfaeff969d261faedb7755adb44c9d
LutyiSan <lucenkoaktavest@gmail.com>
2022-02-07 03:40:50 -0800

    18:55 01.02.22

--- static/js/main.js
+++ static/js/main.js
@@ -1,27 +1,31 @@
 
 function button(id, href){
-let elem = document.querySelector(id)
-elem.addEventListener('click', function(){
-//document.location.href = href;
-window.open(href);
+    let elem = document.querySelector(id)
+    elem.addEventListener('click', function(){
+        window.open(href);
 });
-elem.addEventListener("mouseenter", function(){
-//document.location.href = href;
-elem.style.transform = 'scale(1.1,1.1)';
+    elem.addEventListener("mouseenter", function(){
+    elem.style.transform = 'scale(1.1,1.1)';
 })
-elem.addEventListener("mouseleave", function(){
-//document.location.href = href;
-elem.style.transform = 'scale(1,1)';
-})
-}
+    elem.addEventListener("mouseleave", function(){
+    elem.style.transform = 'scale(1,1)';
+})}
 
-
+function show_hide(elem, target){
+    let el = document.querySelector(elem);
+    let targ = document.querySelector(target)
+    el.addEventListener('click', function(){
+        if (targ.style.display === 'none'){
+            targ.style.display = "block";
+        }else{
+            targ.style.display = "none";
+}})}
 
 
 window.onload = function(){
 button("#telegram", 'https://web.telegram.org');
 button("#instagram", 'https://www.instagram.com/');
 button("#vk", 'https://vk.com/');
-button("#menu", 'https://vk.com/');
+show_hide("#menu", '.menu');
 
 }
